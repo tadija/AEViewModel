4af144efb0dbfaf643296413d0d549a9eac27b05
Marko Tadic <tadija@me.com>
2017-04-09 23:20:02 +0200

    Minor changes

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "TableModel",
     dependencies: [
-        .Package(url: "https://github.com/tadija/convertible.git", majorVersion: 0)
+        .Package(url: "https://github.com/tadija/mappable.git", majorVersion: 0)
     ],
     exclude: ["Example"]
 )
