c5e575c0759f67f04c815c7e01cada0e8d5471cc
Marko Tadic <tadija@me.com>
2017-04-09 13:59:08 +0200

    Restructuring of example project

---
+++ Example/TableModelExample/CustomCell.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+/*
+public class CustomCell: UITableViewCell, MenuCell {
+    
+    let label = UILabel()
+    
+    public override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureUI()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureUI() {
+        contentView.backgroundColor = UIColor.orange
+        
+        label.textColor = .white
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.addSubview(label)
+        
+        label.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+    }
+    
+    public func configure(with item: MenuItem) {
+        label.text = "Hello"
+    }
+    
+}
+*/
---
+++ Example/TableModelExample/SettingsTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  Settings.swift
+//  TableModelExample
+//
+//  Created by Marko Tadić on 4/8/17.
+//  Copyright © 2017 AE. All rights reserved.
+//
+
+import UIKit
+
+/// - TODO: later
+//class SettingsTableViewController: TableViewController {
+//    
+//    
+//    
+//}
+
+//public class TableModelDataSource: MenuViewControllerDelegate {
+//    
+//    public init() {}
+//    
+//    public func cellType(forIdentifier identifier: String) -> UITableViewCell.Type? {
+//        switch identifier {
+//        case "airplaneMode":
+//            return CustomCell.self
+//        default:
+//            //            return SubtitleCell.self
+//            return nil
+//        }
+//    }
+//    
+//}
