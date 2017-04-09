0fd4cf59c1242b3d8000a9d8809fd31f9e90cd63
Marko Tadic <tadija@me.com>
2017-04-09 13:43:00 +0200

    Refactored package into cocoa touch framework project

---
+++ Sources/TableModel.h
@@ -0,0 +1,19 @@
+//
+//  TableModel.h
+//  TableModel
+//
+//  Created by Marko Tadić on 4/8/17.
+//  Copyright © 2017 AE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TableModel.
+FOUNDATION_EXPORT double TableModelVersionNumber;
+
+//! Project version string for TableModel.
+FOUNDATION_EXPORT const unsigned char TableModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TableModel/PublicHeader.h>
+
+
