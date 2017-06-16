c44f8a4768864380bb00466bd10400172f53f8b3
Marko Tadic <tadija@me.com>
2017-06-17 00:59:04 +0200

    Added initial logic to GithubTVMC

--- Example/AEViewModelExample/GithubTVMC.swift
+++ Example/AEViewModelExample/GithubTVMC.swift
@@ -12,10 +12,45 @@ final class GithubTVMC: TableViewModelController {
     
     typealias GithubItem = GithubTable.ItemType
     
+    // MARK: Properties
+    
+    private let dataSource = GithubDataSource()
+    
+    private var repos = [Repo]()
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(refresh(_:)), for: .valueChanged)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let refreshControl = refreshControl {
+            tableView.contentOffset = CGPoint(x: 0, y: -refreshControl.frame.size.height)
+            refreshControl.beginRefreshing()
+            refresh(refreshControl)
+        }
+    }
+    
+    @objc
+    private func refresh(_ sender: UIRefreshControl) {
+        dataSource.reload { (repos) in
+            if let repos = repos {
+                self.repos = repos
+            }
+            sender.endRefreshing()
+        }
+    }
+    
     // MARK: Override
     
     override func cell(forIdentifier identifier: String) -> TableCell {
-        return .basic
+        return .subtitle
     }
     
     override func configureCell(_ cell: TableViewModelCell, at indexPath: IndexPath, with item: Item) {
