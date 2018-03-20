ebab74c7624cbe9f87b9496ac143a897e4c53c92
Marko Tadic <tadija@me.com>
2018-03-21 00:25:22 +0100

    Proof of concept for the potential new approach [WIP]

--- Sources/ViewModel.swift
+++ Sources/ViewModel.swift
@@ -6,6 +6,116 @@
 
 import Foundation
 
+// MARK: - NEW
+
+protocol NewItem {}
+protocol NewSection {
+    var items: [[String : NewItem]] { get set }
+}
+protocol NewDataSource {
+    var sections: [NewSection] { get set }
+}
+
+struct NewBasicItem: NewItem, Codable {
+    let title: String?
+    let detail: String?
+    let image: String?
+    var submodel: NewBasicDataSource?
+}
+struct NewBasicSection: NewSection, Codable {
+    var items: [[String : NewItem]]
+
+    enum CodingKeys: String, CodingKey {
+        case items
+    }
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        items = try container.decode([[String : NewBasicItem]].self, forKey: .items)
+    }
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(items, forKey: .items)
+    }
+}
+struct NewBasicDataSource: NewDataSource, Codable {
+    var sections: [NewSection]
+
+    enum CodingKeys: String, CodingKey {
+        case sections
+    }
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        sections = try container.decode([NewBasicSection].self, forKey: .sections)
+    }
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(sections, forKey: .sections)
+    }
+}
+
+
+
+//protocol NewItem {}
+//protocol NewSection {
+//    associatedtype ItemType: NewItem
+//    var items: [[String : ItemType]] { get set }
+//}
+//protocol NewDataSource {
+//    var sections: [Any] { get set }
+//}
+//
+//extension NewDataSource {
+//    var tmp: [NewSection]? {
+//        return sections as? [NewSection]
+//    }
+//}
+//
+//struct NewBasicItem: NewItem, Codable {
+//    let title: String?
+//    let detail: String?
+//}
+//struct NewBasicSection: NewSection, Codable {
+//    var items: [[String : NewBasicItem]]
+//}
+//struct NewBasicDataSource: NewDataSource, Codable {
+//    var sections: [Any]
+//
+//    enum CodingKeys: String, CodingKey {
+//        case sections
+//    }
+//    init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        sections = try container.decode([NewBasicSection].self, forKey: .sections)
+//    }
+//    func encode(to encoder: Encoder) throws {
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//        try container.encode(sections, forKey: .sections)
+//    }
+//}
+
+
+//protocol NewItem {}
+//protocol NewSection {
+//    associatedtype ItemType: NewItem
+//    var items: [[String : ItemType]] { get set }
+//}
+//protocol NewDataSource {
+//    var sections: [NewSection] { get set }
+//}
+//
+//struct CodableSection: NewSection, Codable {
+//    var header: String?
+//    var footer: String?
+//    var items: [[String : CodableItem]]
+//}
+//
+//struct CodableItem: NewItem, Codable {
+//    let identifier: String
+//    var data: [String : String]
+//}
+
+// MARK: - OLD
+
 // MARK: - Protocols
 
 public protocol ViewModel {}
